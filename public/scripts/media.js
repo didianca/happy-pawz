010eb5e7f5659016bf85e12898e559aadabe39b3
didianca <cazaranka@gmail.com>
2019-05-01 18:20:12 -0400

    implemented email button for media.html

--- public/scripts/media.js
+++ public/scripts/media.js
@@ -1,28 +1,7 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
 
-
-$(document).ready(function () {
-    let $overlay = $('div#overlay');
-    let $image = $("img");
-
-//An image to overlay
-    $overlay.append($image);
-
-//Add overlay
-    $("body").append($overlay);
-
-//click the image and a scaled version of the full size image will appear
-    $("#photo-gallery a").click(function (event) {
-        event.preventDefault();
-        let imageLocation = $(this).attr("href");
-
-        //update overlay with the image linked in the link
-        $image.attr("src", imageLocation);
-
-        //show the overlay
-        $overlay.show();
-    });
-
-    $("#overlay").click(function () {
-        $("#overlay").hide();
-    });
-});
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
