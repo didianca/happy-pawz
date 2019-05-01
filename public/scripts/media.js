1ab496cf03a3b66ef3380d4e5d2c4eacbb879855
didianca <cazaranka@gmail.com>
2019-05-01 16:00:17 -0400

    working on media page

---
+++ public/scripts/media.js
@@ -0,0 +1,25 @@
+
+let overlay = $('div.overlay');
+let $image = $('img');
+
+//An image to overlay
+overlay.append($image);
+
+//Add overlay
+$("body").append($overlay);
+
+//click the image and a scaled version of the full size image will appear
+$("#photo-gallery a").click( function(event) {
+    event.preventDefault();
+    let imageLocation = $(this).attr("href");
+
+    //update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+    //show the overlay
+    $overlay.show();
+} );
+
+$("#overlay").click(function() {
+    $( "#overlay" ).hide();
+});
