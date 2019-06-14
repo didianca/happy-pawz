2f957ba87afb9001c6141622194007ad9239706a
didianca <cazaranka@gmail.com>
2019-06-14 23:49:49 +1200

    added comments(instructions) to login endpoint (/api/routes/auth)

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,3 +1,4 @@
+//import all needed packages/modules
 const validate= require('../middleware/validate');
 const{User}=require('../models/user');
 const express = require('express');
@@ -5,18 +6,26 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
 
-
 //Login
 router.post('/', validate(validateInput),async (req, res) => {
+    /* EITHER IF IT IS AN EMAIL OR A PASSWORD PROBLEM,
+    THE USER SHOULD BE GIVEN A VAGUE RESPONSE SUCH AS "invalid e-mail or password."
+    SO THEY CAN NOT CHEAT IT*/
+    //check for email && pass -->> if valid => authenticate through auth-token
+
+    //check for email
     let user = await User.findOne({email: req.body.email});
     if(!user) return res.status(400).send('Invalid e-mail or password.');
-    /* EITHER IF IT IS AN EMAIL OR A PASSWORD PROBLEM, THE USER SHOULD BE GIVEN A VAGUE RESPOND SUCH AS "invalid e-mail or password."*/
+    //check for pass
     const validPassword = await bcrypt.compare(req.body.password,user.password);
     if(!validPassword) return res.status(400).send('Invalid e-mail or password.');
+    //generate token which provides authentication
     const token =  user.generateAuthToken();
     res.send(token);
 });
-
+//validate user input through Joi npm package
+//different than the User validation method because this one requires just email and pass so phone number and name is redundant
+//this function gets some input and compares it to the db rather than re-post a new user there for it needs a diff validation
 function validateInput(req) {
     const schema = {
         email:Joi.string().min(5).max(255).required().email(),
@@ -24,7 +33,6 @@ function validateInput(req) {
     };
     return Joi.validate(req, schema);
 }
-
-
-
+//export route to be implemented
+//see /api/startup/routes
 module.exports = router;
