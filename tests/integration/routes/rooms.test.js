c54cd71a7a1cac164b06f8bded5878416d783fe2
didianca <cazaranka@gmail.com>
2019-07-25 23:05:56 -0300

    Revert "implemented integration tests for the GET by id operation on the rooms collection"
    This reverts commit 22b1614f

--- tests/integration/routes/rooms.test.js
+++ tests/integration/routes/rooms.test.js
@@ -3,7 +3,6 @@ const request = require('supertest');
 const {Room} = require('../../../api/models/room');
 const {Employee} = require('../../../api/models/employee');
 const {User} = require('../../../api/models/user');
-const {Role} = require('../../../api/models/role');
 const mongoose = require('mongoose');
 let server;
 describe('/api/rooms route', () => {
@@ -66,7 +65,7 @@ describe('/api/rooms route', () => {
             room.setOutdoorAccess();
             room.setDailyRentalRateAndName();
             await room.save();
-            const id = '1';
+            const id ='1';
             const res = await request(server)
                 .get(`/api/rooms/${id}`);
             expect(res.status).toBe(404);
@@ -91,7 +90,7 @@ describe('/api/rooms route', () => {
             room.setOutdoorAccess();
             room.setDailyRentalRateAndName();
             await room.save();
-            const id = mongoose.Types.ObjectId();
+            const id =mongoose.Types.ObjectId();
             const res = await request(server)
                 .get(`/api/rooms/${id}`);
             expect(res.status).toBe(404);
@@ -116,51 +115,15 @@ describe('/api/rooms route', () => {
             room.setOutdoorAccess();
             room.setDailyRentalRateAndName();
             await room.save();
-            const id = room._id;
+            const id =room._id;
             const res = await request(server)
                 .get(`/api/rooms/${id}`);
             expect(res.status).toBe(200);
-            expect(res.body).toHaveProperty('size', 'single');
-            expect(res.body).toHaveProperty('level', 0);
+            expect(res.body).toHaveProperty('size','single');
+            expect(res.body).toHaveProperty('level',0);
             expect(res.body).toHaveProperty('caretaker');
             expect(res.body).toHaveProperty('maid');
-            expect(res.body).toHaveProperty('dailyRentalRate', 250);
-        });
-    });
-    describe('POST /', () => {
-        let token;
-        let caretaker;
-        let maid;
-        const exec = async () => {
-            return await request(server)
-                .post('/api/rooms')
-                .set('x-auth-token', token)
-                .send({
-                    size: 'single',
-                    level: '0',
-                    caretakerId: caretaker._id,
-                    maidId: maid._id
-                })
-        };
-        beforeEach(async () => {
-            caretaker = new Employee({
-                name: 'caretaker name',
-                role: new Role({title:'caretaker'}),
-                phone: '12345'
-            });
-            await caretaker.save();
-            maid = new Employee({
-                name: 'maid name',
-                role: new Role({title:'maid'}),
-                phone: '12345'
-            });
-            await maid.save();
-            token=new User({isAdmin: true}).generateAuthToken();
-        });
-        it('should return 401 if client is not logged in',async ()=>{
-            token = '';
-            const res = await exec();
-            expect(res.status).toBe(401);
+            expect(res.body).toHaveProperty('dailyRentalRate',250);
         });
     });
 });
